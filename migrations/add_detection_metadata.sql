dacebe78543038d2561627f54259d56d09282fcc
Your Name <you@example.com>
2025-01-27 23:00:28 -0600

    chore: Remove old migration files
    Migration files have been consolidated into numbered migrations

--- migrations/add_detection_metadata.sql
+++
@@ -1,8 +0,0 @@
--- Add new columns to detections table
-ALTER TABLE detections ADD COLUMN category_name TEXT;
-ALTER TABLE detections ADD COLUMN camera_name TEXT;
-ALTER TABLE detections ADD COLUMN detection_index INTEGER;
-
--- Update existing rows with default values where needed
-UPDATE detections SET category_name = 'bird' WHERE category_name IS NULL;
-UPDATE detections SET detection_index = 0 WHERE detection_index IS NULL;
